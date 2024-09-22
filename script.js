c8be56172dc2590e68c358f611937ce3dd28f7b4
Wisdom Matthew <wisdommatthew08@gmail.com>
2024-09-22 16:41:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// ALWAYS USE STRICT
+"use strict";
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
